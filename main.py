418f1440417b30c5a917b4adde7ccfb2baaca32a
Ben Nageris <bennageris@gmail.com>
2023-06-13 23:40:23 +0300

    add crossover and mutations

--- main.py
+++ main.py
@@ -1,26 +1,51 @@
-import matplotlib
 import numpy as np
-import matplotlib.pyplot as plt
-from sklearn.datasets import make_blobs
 from sklearn.metrics import accuracy_score
 from sklearn.preprocessing import OneHotEncoder
-from sklearn.model_selection import train_test_split
 
 from dataset import DataSet
 from nn_network import FFSN_MultiClass
+from nn_genetic import FFSN_GeneticMultiClass
 
 if __name__ == "__main__":
+    nn_0 = DataSet.parse_nn_file(r"nn0.txt")
+    genetic_nn_1 = FFSN_GeneticMultiClass(
+        n_inputs=4,
+        hidden_sizes=[4, 2],
+        # hidden_sizes=[16, 8, 4],
+        n_outputs=2,
+    )
+    print(f"nn1:{genetic_nn_1.W}")
+    genetic_nn_2 = FFSN_GeneticMultiClass(
+        n_inputs=4,
+        hidden_sizes=[4, 2],
+        # hidden_sizes=[16, 8, 4],
+        n_outputs=2,
+    )
+    print(f"nn2:{genetic_nn_2.W}")
+
+    genetic_crossover_nn_3 = FFSN_GeneticMultiClass.crossover(genetic_nn_1, genetic_nn_2)
+    print(f"nn3:{genetic_crossover_nn_3.W}")
+
+    # print(genetic_nn._n_neurons)
+    # layer, x, y = genetic_nn.get_neuron_location(423)
+
+    # genetic_nn.mutation()
+
+
+    # print(genetic_algo.W)
+    """
     enc = OneHotEncoder()
 
     # exercise
-    nn_0 = DataSet.parse_nn_file(r"nn0.txt")
+    nn_0 = DataSet.parse_nn_file(r"nn1.txt")
+    # nn_0 = DataSet.parse_nn_file(r"nn1.txt")
     x_train, x_val, y_train, y_val = nn_0.train_test_split(p_train=0.6)
     print(len(x_train), len(x_val), len(y_train), len(y_val))
 
     ffsn_multi = FFSN_MultiClass(n_inputs=16, hidden_sizes=[16, 8, 4], n_outputs=2)
 
     y_OH_train = enc.fit_transform(np.expand_dims(y_train, 1)).toarray()
-    ffsn_multi.fit(x_train, y_OH_train, epochs=3000, learning_rate=.005, display_loss=True)
+    ffsn_multi.fit(x_train, y_OH_train, epochs=1000, learning_rate=.005, display_loss=True)
 
     Y_pred_train = ffsn_multi.predict(x_train)
     Y_pred_train = np.argmax(Y_pred_train, 1)
@@ -33,38 +58,4 @@ if __name__ == "__main__":
 
     print("Training accuracy", round(accuracy_train, 2))
     print("Validation accuracy", round(accuracy_val, 2))
-    """
-    # base
-    data, labels = make_blobs(n_samples=1000, n_features=2, centers=4, random_state=0)
-    # print(data.shape, labels.shape)
-    ffsn_multi = FFSN_MultiClass(n_inputs=2, hidden_sizes=[2, 3], n_outputs=4)
-    X_train, X_val, Y_train, Y_val = train_test_split(data, labels, stratify=labels, random_state=0)
-    print(type(X_train))
-    print(X_train.shape)
-    
-    # print(X_train.shape, X_val.shape)
-    # 0 -> (1, 0, 0, 0), 1 -> (0, 1, 0, 0), 2 -> (0, 0, 1, 0), 3 -> (0, 0, 0, 1)
-    y_OH_train = enc.fit_transform(np.expand_dims(Y_train, 1)).toarray()
-    # y_OH_val = enc.fit_transform(np.expand_dims(Y_val, 1)).toarray()
-
-    ffsn_multi.fit(X_train, y_OH_train, epochs=100, learning_rate=.005, display_loss=True)
-    """
-
-    """
-    Y_pred_train = ffsn_multi.predict(X_train)
-    Y_pred_train = np.argmax(Y_pred_train, 1)
-
-    Y_pred_val = ffsn_multi.predict(X_val)
-    Y_pred_val = np.argmax(Y_pred_val, 1)
-
-    accuracy_train = accuracy_score(Y_pred_train, Y_train)
-    accuracy_val = accuracy_score(Y_pred_val, Y_val)
-
-    print("Training accuracy", round(accuracy_train, 2))
-    print("Validation accuracy", round(accuracy_val, 2))
-
-    my_cmap = matplotlib.colors.LinearSegmentedColormap.from_list("", ["red", "yellow", "green"])
-    plt.scatter(X_train[:, 0], X_train[:, 1], c=Y_pred_train, cmap=my_cmap,
-                s=15 * (np.abs(np.sign(Y_pred_train - Y_train)) + .1))
-    plt.show()
     """
---
+++ nn_genetic.py
@@ -0,0 +1,165 @@
+from __future__ import annotations
+from typing import List, Dict, Optional
+import tqdm
+import pickle
+import random
+import itertools
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+class FFSN_GeneticMultiClass:
+    def __init__(
+            self,
+            n_inputs: int,
+            n_outputs: int,
+            hidden_sizes: List[int],
+            mutation_p: float = 0.05,
+            max_mutation_diff: float = 0.2,
+            w: Optional[Dict[int, np.array]] = None,
+            b: Optional[Dict[int, np.array]] = None):
+        self.nx = n_inputs
+        self.ny = n_outputs
+        self.hidden_sizes = hidden_sizes
+        self.nh = len(hidden_sizes)
+        self._max_mutation_diff = max_mutation_diff
+        self.sizes = [self.nx] + hidden_sizes + [self.ny]
+        self._w_layer_size = {}
+
+        if w is None or b is None:
+            # randomize if bias or weight isn't passed
+            self.W = {}
+            self.B = {}
+            self._n_neurons = 0
+            for i in range(self.nh + 1):
+                self.W[i + 1] = np.random.randn(self.sizes[i], self.sizes[i + 1])
+                self._n_neurons += self.W[i + 1].size
+                self.B[i + 1] = np.zeros((1, self.sizes[i + 1]))
+                self._w_layer_size[i + 1] = self.sizes[i] * self.sizes[i + 1]
+        else:
+            self.W = w
+            self.B = b
+            for i in range(self.nh + 1):
+                self._w_layer_size[i + 1] = self.sizes[i] * self.sizes[i + 1]
+        self._w_layers_idx = list(self.W.keys())
+        self._max_layer_idx = max(self._w_layers_idx)
+        self._mutation_p = mutation_p
+
+    def dump(self, path: str) -> None:
+        with open(path, "wb") as f:
+            pickle.dump(self, f)
+
+    @staticmethod
+    def load(path: str) -> FFSN_GeneticMultiClass:
+        with open(path, "rb") as f:
+            return pickle.load(f)
+
+    @staticmethod
+    def sigmoid(x):
+        return 1.0 / (1.0 + np.exp(-x))
+
+    @staticmethod
+    def softmax(x):
+        exps = np.exp(x)
+        return exps / np.sum(exps)
+
+    def forward_pass(self, x):
+        self.A = {}
+        self.H = {}
+        self.H[0] = x.reshape(1, -1)
+        for i in range(self.nh):
+            self.A[i + 1] = np.matmul(self.H[i], self.W[i + 1]) + self.B[i + 1]
+            self.H[i + 1] = self.sigmoid(self.A[i + 1])
+        self.A[self.nh + 1] = np.matmul(self.H[self.nh], self.W[self.nh + 1]) + self.B[self.nh + 1]
+        self.H[self.nh + 1] = self.softmax(self.A[self.nh + 1])
+        return self.H[self.nh + 1]
+
+    def predict(self, X):
+        Y_pred = []
+        for x in X:
+            y_pred = self.forward_pass(x)
+            Y_pred.append(y_pred)
+        return np.array(Y_pred).squeeze()
+
+    @staticmethod
+    def grad_sigmoid(x):
+        return x * (1 - x)
+
+    def mutation(self):
+        mut = random.random()
+        if mut < self._mutation_p:
+            randomized_layer = random.choice(self._w_layers_idx)
+            layer = []
+            for layer_weights in self.W[randomized_layer]:
+                tmp_layer = []
+                for neuron in layer_weights:
+                    d = random.uniform(-1 * self._max_mutation_diff, self._max_mutation_diff)
+                    tmp_layer.append(neuron + d)
+                layer.append(tmp_layer)
+            self.W[randomized_layer] = np.array(layer)
+
+    @staticmethod
+    def crossover(nn_1: FFSN_GeneticMultiClass, nn_2: FFSN_GeneticMultiClass) -> FFSN_GeneticMultiClass:
+        assert nn_1._n_neurons == nn_2._n_neurons, (f"number of neurons don't match nn_1:"
+                                                    f"{nn_1._n_neurons}, nn_2:{nn_2._n_neurons}")
+        print(f"n neurons:{nn_1._n_neurons}")
+        print(f"layers:{nn_1.W.keys()}")
+        # split_idx = random.randint(0, nn_1._n_neurons - 1)
+        split_idx = 28
+        print(f"{split_idx=}")
+        if split_idx == nn_1._n_neurons:
+            # copy all from nn_1
+            layer_idx, (x_loc, y_loc) = nn_1._max_layer_idx, nn_1.W[nn_1._max_layer_idx].shape
+        else:
+            layer_idx, x_loc, y_loc = nn_1.get_neuron_location(neuron_idx=split_idx)
+        print(f"{layer_idx=}, {x_loc=},{y_loc=}")
+        w = {}
+        # copy until the split layer - nn1
+        for layer in range(1, layer_idx):
+            w[layer] = nn_1.W[layer]
+        # split layer copy - nn1 & nn2
+        print(f"{layer_idx}")
+        split_layer_x_size, split_layer_y_size = nn_1.W[layer_idx].shape
+        found = False
+        layer = []
+        for x in range(split_layer_x_size):
+            tmp_layer = []
+            for y in range(split_layer_y_size):
+                if x == x_loc and y == y_loc:
+                    found = True
+                nn_to_query = nn_2 if found else nn_1
+                tmp_layer.append(nn_to_query.W[layer_idx][x][y])
+            layer.append(tmp_layer)
+        w[layer_idx] = np.array(layer)
+        # post split layers - nn2
+        for layer in range(layer_idx + 1, nn_2._max_layer_idx + 1):
+            w[layer] = nn_2.W[layer]
+
+        return FFSN_GeneticMultiClass(
+            n_inputs=nn_1.nx,
+            hidden_sizes=nn_1.hidden_sizes,
+            n_outputs=nn_2.ny,
+            w=w,
+            b={}
+        )
+
+    def get_neuron_location(self, neuron_idx: int):
+        layers = sorted(self.W.keys())
+        n_neurons_passed = 0
+        for layer in layers:
+            n_current_layer = self.W[layer].size
+            if neuron_idx < n_neurons_passed + n_current_layer:
+                idx = neuron_idx - n_neurons_passed
+                layer_x_size, layer_y_size = self.W[layer].shape
+                return layer, int(idx / layer_y_size), idx % layer_y_size
+            n_neurons_passed += n_current_layer
+        return None
+
+    @staticmethod
+    def cross_entropy(label, pred):
+        yl = np.multiply(pred, label)
+        yl = yl[yl != 0]
+        yl = -np.log(yl)
+        yl = np.mean(yl)
+        return yl
+
